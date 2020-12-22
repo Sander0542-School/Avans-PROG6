16af220953d13742f22f78b90d4e8b4181d5d0aa
Sander Jochems <sanderjochems@hotmail.nl>
2020-12-22 19:38:06 +0100

    Initial commit

---
+++ ShipsInSpace.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShipsInSpace.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
