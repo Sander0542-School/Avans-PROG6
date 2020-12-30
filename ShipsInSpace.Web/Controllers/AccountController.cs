ce57e0e9a14936b356842ed69daffb3ad5e69d66
Sander Jochems <sanderjochems@hotmail.nl>
2020-12-30 17:29:12 +0100

    Create AccountController

---
+++ ShipsInSpace.Web/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShipsInSpace.Data.Models;
+using ShipsInSpace.Web.Models.Account;
+
+namespace ShipsInSpace.Web.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Entry point into the login workflow
+        /// </summary>
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        /// <summary>
+        /// Handle postback from username/password login
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            model.ReturnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, true, false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect(model.ReturnUrl);
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            
+            return LocalRedirect(returnUrl);
+        }
+
+        public async Task<IActionResult> AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShipsInSpace.Web/Models/Account/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShipsInSpace.Web.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
