013a8cc8d396f1c923c9599ef74b01fd2988f337
Tommy Hosewol <t.hosewol@gmail.com>
2021-01-06 14:19:44 +0100

    UI

--- ShipsInSpace.Web/Models/Ship/HullEngineViewModel.cs
+++ ShipsInSpace.Web/Models/Ship/HullEngineViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using GalacticSpaceTransitAuthority;
 using ShipsInSpace.Logic.Validators.Attributes;
